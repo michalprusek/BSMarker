654a6278ea55dd555768ca04cbef4e3e9f849db4
Lukáš Veškrna <lukas.veskrna@gmail.com>
2025-03-21 17:27:41 +0100

    Improved wound finding algorithm

--- backend/imgproc/wound.py
+++ backend/imgproc/wound.py
@@ -3,7 +3,7 @@ import numpy as np
 
 
 def prewitt(img, thresh):
-    """Perform a Prewitt edge detection filter"""
+    """Perform a modified (incl. diagonal) Prewitt edge detection filter"""
     temp = img.astype(np.float32)
     m1 = np.array([
         [-1,  0,  1], 
@@ -24,24 +24,30 @@ def prewitt(img, thresh):
     return (edges1 | edges2).astype(np.uint8) * 255
 
 
-def edges(img):
+def blur(img, blur_size=3):
+    return cv.blur(img, (3, 3))
+
+
+def edges(img, blur_size=3, edge_threshold=8):
     """Blur + edge detect"""
-    img_blur = cv.blur(img, (3, 3))
-    return prewitt(img_blur, 10)
+    img_blur = blur(img, blur_size=blur_size)
+    return prewitt(img_blur, edge_threshold)
 
 
-def wound_mask(img):
+def wound_mask(img, blur_size=3, edge_threshold=8, morphology_size=40):
     """Find the mask of the wound"""
     return cv.morphologyEx(
-        edges(img), 
+        edges(img, blur_size=blur_size, edge_threshold=edge_threshold), 
         cv.MORPH_CLOSE, 
-        cv.getStructuringElement(cv.MORPH_ELLIPSE, (30, 30))
+        cv.getStructuringElement(cv.MORPH_ELLIPSE, (morphology_size, morphology_size))
     )
 
 
-def wound_contours(img, approx=None, threshold=0.005):
+def wound_contours(img=None, mask=None, approx=None, min_size_threshold=0.005):
     """Find the contours of the wound"""
-    mask = wound_mask(img)
+    if mask is None:
+        assert img is not None, "wound_contours requires img or mask"
+        mask = wound_mask(img)
 
     contours, _ = cv.findContours(
         cv.bitwise_not(mask), 
@@ -49,13 +55,16 @@ def wound_contours(img, approx=None, threshold=0.005):
         cv.CHAIN_APPROX_SIMPLE
     )
 
-    central_contours = [c for c in contours if cv.contourArea(c) >= threshold*img.shape[0]*img.shape[1]]
+    contours = [
+        c for c in contours 
+        if cv.contourArea(c) >= min_size_threshold*img.shape[0]*img.shape[1]
+    ]
 
     if approx is not None:
-        for i, c in enumerate(central_contours):
-            central_contours[i] = cv.approxPolyDP(c, approx, True)
+        for i, c in enumerate(contours):
+            contours[i] = cv.approxPolyDP(c, approx, True)
 
-    return [c.reshape((c.shape[0], 2)) for c in central_contours]
+    return [c.reshape((c.shape[0], 2)) for c in contours]
 
 
 def cut_out(img, contours):
@@ -65,19 +74,35 @@ def cut_out(img, contours):
     return img * (mask == 255)
 
 
-def free_cells(img, contours, approx=None, min_area=240):
-    """Locate free cells inside a wound contour with area >= min_area"""
+def free_cells(img, contours, approx=None, min_size_threshold=0.00005, edge_threshold=10, open_size=3, close_size=30):
+    """Locate free cells inside a wound contour with area >= min_size_threshold*img.shape[0]*img.shape[1]"""
 
     wound = cut_out(img, contours)
-    edges_inner = prewitt(wound, 10)
-    outline = cv.drawContours(np.zeros(img.shape, np.uint8), contours, -1, 255, 2)
+    edges_inner = prewitt(wound, edge_threshold)
+    outline = cv.drawContours(
+        np.zeros(img.shape, np.uint8), 
+        contours, 
+        -1, 255, 2
+    )
 
     inner = edges_inner-outline
-    inner = cv.morphologyEx(inner, cv.MORPH_OPEN, cv.getStructuringElement(cv.MORPH_ELLIPSE, (3, 3)))
+    inner = cv.morphologyEx(
+        inner, 
+        cv.MORPH_OPEN, 
+        cv.getStructuringElement(cv.MORPH_ELLIPSE, (open_size, open_size))
+    )
     _, inner = cv.threshold(inner, 127, 255, cv.THRESH_BINARY)
 
-    morph_inner = cv.morphologyEx(inner, cv.MORPH_CLOSE, cv.getStructuringElement(cv.MORPH_ELLIPSE, (30, 30)))
+    morph_inner = cv.morphologyEx(
+        inner, 
+        cv.MORPH_CLOSE, 
+        cv.getStructuringElement(cv.MORPH_ELLIPSE, (close_size, close_size))
+    )
 
     contours_inner, _ = cv.findContours(morph_inner, cv.RETR_EXTERNAL, cv.CHAIN_APPROX_SIMPLE)
 
-    return [(cv.approxPolyDP(c, approx, True)[:, 0] if approx else c[:, 0]) for c in contours_inner if cv.contourArea(c) > min_area]
+    return [
+        (cv.approxPolyDP(c, approx, True)[:, 0] if approx else c[:, 0]) 
+        for c in contours_inner 
+        if cv.contourArea(c) >= min_size_threshold*img.shape[0]*img.shape[1]
+    ]
