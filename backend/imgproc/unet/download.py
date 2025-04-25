e83cd41c45e6530d5622073a931802258f53e362
Lukáš Veškrna <lukas.veskrna@gmail.com>
2025-04-26 00:18:38 +0200

    Better dataset organization and download script

--- backend/imgproc/unet/download.py
+++ backend/imgproc/unet/download.py
@@ -6,54 +6,84 @@ import os
 import tqdm
 
 
-URL = "http://193.86.114.62:8000/"
+URL = ""
 USER = "admin"
 PASS = "adminadmin"
-PROJECT_ID = "5b622c5b-4853-4e77-99b1-87e1f566507a"
+
+DATASET = [
+	("train", [
+		["9f92f1d9-86d6-44ba-a8f8-e70ca486dd3a", slice(150)],        # U2OS, Ex 01
+		["9f0accb0-5479-48a7-bcac-9645a7f17a3b", slice(25)],         # U2OS, Ex 14
+
+		["ca5a5496-6b97-4254-ad5d-1ce20f9d0dca", slice(None)],       # MiaPaca-2, Ex 02
+
+		["a5272f7c-65d3-41c1-850d-8e8a365190b0", slice(42)],         # UFH-001, E02_C6
+
+		["cc2f4a99-f614-4081-8a99-e292d2744ef1", slice(28)],         # MRC-5, E02_C6
+		["cc2f4a99-f614-4081-8a99-e292d2744ef1", slice(30, 231, 20)],# MRC-5, E02_C6
+	]),
+	("test", [
+		["977f2bd2-f8b1-4eef-a580-e625040bace5", slice(None)],       # MiaPaca-2, Ex 01
+	]),
+]
 DATASET_PATH = pathlib.Path("dataset/")
-IMAGE_DIR = "imgs"
+IMAGE_DIR = "images"
 MASK_DIR = "masks"
 
 
-if not os.path.exists(DATASET_PATH):
-	os.makedirs(DATASET_PATH, exist_ok=True)
-	os.makedirs(DATASET_PATH / IMAGE_DIR, exist_ok=True)
-	os.makedirs(DATASET_PATH / MASK_DIR, exist_ok=True)
-assert URL.endswith("/")
+def init_paths():
+	if not os.path.exists(DATASET_PATH):
+		os.makedirs(DATASET_PATH, exist_ok=True)
+		for subset, _ in DATASET:
+			os.makedirs(DATASET_PATH / subset, exist_ok=True)
+			os.makedirs(DATASET_PATH / subset / IMAGE_DIR, exist_ok=True)
+			os.makedirs(DATASET_PATH / subset / MASK_DIR, exist_ok=True)
+	assert URL.endswith("/")
 
-s = requests.Session()
-s.post(URL + "graphql/", json={"query": "mutation { login(username: \"" + USER + "\", password: \"" + PASS + "\") { username } }"})
-res = s.post(URL + "graphql/", json={"query": """
-{
-  project(id: \"""" + PROJECT_ID + """\") {
-    experiments {
-      name,
-      frames {
-        id,
-        image: data {
-          url
-        },
-        mask: data(mask: true) {
-          url
+
+def fetch_experiment(s, eid):
+	s.post(URL + "graphql/", json={"query": "mutation { login(username: \"" + USER + "\", password: \"" + PASS + "\") { username } }"})
+	query = """
+	{
+      experiment(id: \"""" + eid + """\") {
+        name,
+        frames {
+          number,
+          image: data {
+            url
+          },
+          mask: data(mask: true) {
+            url
+          }
         }
       }
-    }
-  }
-}"""
-})
-experiments = res.json()["data"]["project"]["experiments"]
+	}"""
+	res = s.post(URL + "graphql/", json={"query": query})
+	assert res.json()["data"] is not None, query + "\nerror: " + repr(res.json()["errors"])
 
-for experiment in experiments:
-	print(f"Experiment {experiment['name']}")
-	for frame in tqdm.tqdm(experiment["frames"]):
-		fname = os.path.basename(urlparse(frame["image"]["url"]).path)
-		if not fname:
-			fname = frame["id"] + ".jpg"
+	return res.json()["data"]["experiment"]
 
-		res = s.get(URL + frame["image"]["url"], stream=True)
-		with open(DATASET_PATH / IMAGE_DIR / fname, "wb") as file:
-			shutil.copyfileobj(res.raw, file)
 
-		res = s.get(URL + frame["mask"]["url"], stream=True)
-		with open(DATASET_PATH / MASK_DIR / fname, "wb") as file:
-			shutil.copyfileobj(res.raw, file)
+def main():
+	init_paths()
+	s = requests.Session()
+
+	for subset, composition in DATASET:
+		for eid, sl in composition:
+			experiment = fetch_experiment(s, eid)
+
+			print(f"Experiment {experiment["name"]}")
+			for frame in tqdm.tqdm(experiment["frames"][sl]):
+				fname = f"{experiment["name"]}_{frame["number"]:03}.jpg"
+
+				res = s.get(URL + frame["image"]["url"], stream=True)
+				with open(DATASET_PATH / subset / IMAGE_DIR / fname, "wb") as file:
+					shutil.copyfileobj(res.raw, file)
+
+				res = s.get(URL + frame["mask"]["url"], stream=True)
+				with open(DATASET_PATH / subset / MASK_DIR / fname, "wb") as file:
+					shutil.copyfileobj(res.raw, file)
+
+
+if __name__ == "__main__":
+	main()
